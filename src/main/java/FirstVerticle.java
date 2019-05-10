c4bfeabfd1c819230a2ce83f72ae59fd519d1bf4
Roy Munialo Wati <munialo.roy@ekenya.co.ke>
2019-05-10 14:25:43 +0300

    this commit a simple implementation of vert.x web lib. Routing a request to a function

--- src/main/java/FirstVerticle.java
+++ src/main/java/FirstVerticle.java
@@ -1,17 +1,28 @@
+import io.netty.handler.codec.http.HttpResponse;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class FirstVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> future) throws Exception {
         super.start();
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        router.route("/produce-string").handler(event -> {
+           // CONFIGURING THE INTENDED RESPONSE
+            HttpServerResponse response = event.response();
+            response.putHeader("content-type","text/html");
+            response.end("this is a string produced by a vert.x application");
+        });
+
         vertx.createHttpServer()
-                .requestHandler(httpServerRequest -> {
-                    httpServerRequest.response()
-                            .end("verticle server deployed");
-                })
-                .listen(config().getInteger("server.port",8810),  // Reads from a configuration file
+                .requestHandler(router::accept)  // routing our request to our intended response
+
+                .listen(config().getInteger("server.port",8801),  // Reads from a configuration file
                         httpServerAsyncResult -> {
                     if(httpServerAsyncResult.succeeded())
                         future.complete();
